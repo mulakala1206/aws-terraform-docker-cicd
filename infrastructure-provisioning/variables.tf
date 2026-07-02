b5118ab2f08096189f9bb99da16e6e6ba8d48acf
mulakala1206 <mulakala1206@gmail.com>
2026-07-02 17:22:02 +0530

    modified instance type

--- infrastructure-provisioning/variables.tf
+++ infrastructure-provisioning/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 Instance Type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "key_name" {
